b0179f8d0d66bf33d1bbb5d91f6b5801c593b51e
Nicolas Viana da Silva <nicolas.viana.silva@outlook.com.br>
2018-11-22 16:33:45 -0200

    início ajustes

---
+++ ASP.NET Hash Generator/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET_Hash_Generator.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Hash_Generator.Controllers
+{
+    [Produces("application/json"), Route("api/home"), ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] InputPasswordViewModel inputPasswordViewModel)
+        {
+            var passwordHasher = new PasswordHasher<string>();
+            return Ok(passwordHasher.HashPassword("", inputPasswordViewModel.Password));
+        }
+    }
+}
---
+++ ASP.NET Hash Generator/ViewModel/InputPasswordViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ASP.NET_Hash_Generator.ViewModel
+{
+    public class InputPasswordViewModel
+    {
+        public string Password { get; set; }
+    }
+}
